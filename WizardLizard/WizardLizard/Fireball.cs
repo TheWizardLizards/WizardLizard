88e46884a074643bc42e6db98ee4a437baaac221
NielsJustesen <niels449@hotmail.com>
2016-05-16 09:54:07 +0200

    Playermovement

---
+++ WizardLizard/WizardLizard/Fireball.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace WizardLizard
+{
+    class Fireball : Component, ILoadable, IUpdateable, IAnimateable, ICollisionEnter, ICollisionExit
+    {
+        private Transform transform;
+        private Animator animator;
+        public Fireball(GameObject gameObject) : base (gameObject)
+        {
+            animator = (Animator)GameObject.GetComponent("Animator");
+            transform = gameObject.Transform;
+        }
+        public void Update()
+        {
+        }
+        public void LoadContent(ContentManager content)
+        {
+        }
+
+        public void OnAnimationDone(string animationName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCollisionEnter(Collider other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCollisionExit(Collider other)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
