e9d487a56e5e4e0f1fb256686c1497252218d25b
NielsJustesen <niels449@hotmail.com>
2016-06-06 12:34:51 +0200

    sovs

--- WizardLizard/UnitTest/UnitTest1.cs
+++ WizardLizard/UnitTest/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace TestWizardLizard
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestHealthGlobe()
         {
         }
     }
